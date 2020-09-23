81cc0ed1139f892e735b97979dbecb8684004e2e
Єлизавета Клименко <lizaklym@gmail.com>
2020-09-24 00:18:31 +0300

    Добавьте файлы проекта.

---
+++ Game15(1)/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game15_1_
+{
+    public partial class Form1 : Form
+    {
+
+        Game game; //описываем переменную game
+        public Form1()
+        {
+            InitializeComponent();
+            game = new Game(4);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+        }
+        private void button10_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+        }
+        private void button11_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+        }
+        private void button12_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+        }
+        private void button13_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+        }
+        private void button15_Click(object sender, EventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.shift(position);
+            refresh();
+            if (game.check_numbers())
+            {
+                MessageBox.Show("Вы победили!");
+                start_game();
+            }
+         
+        }
+        private Button button (int position)
+        {
+            switch (position)
+            {
+                case 0: return button0;
+                case 1: return button1;
+                case 2: return button2;
+                case 3: return button3;
+                case 4: return button4;
+                case 5: return button5;
+                case 6: return button6;
+                case 7: return button7;
+                case 8: return button8;
+                case 9: return button9;
+                case 10: return button10;
+                case 11: return button11;
+                case 12: return button12;
+                case 13: return button13;
+                case 14: return button14;
+                case 15: return button15;
+
+                default: return null;
+            }
+        }
+
+        //Ункция, вызывает начало игры и перемешивает "фишки" местами
+       private void Form1_Load(object sender, EventArgs e)
+        {
+            start_game();
+
+        }
+
+
+        private void menustart_Click(object sender, EventArgs e)
+        {
+            start_game();
+        }
+
+        private void start_game()
+        {
+              game.start();
+            for (int j=0; j<100; j++)
+                game.shift_random();
+            refresh();
+        }
+
+        //Функция,которая обновляет содержимое кнопок
+        private void refresh()
+        {
+            for (int position = 0; position < 16; position++)
+            {
+                int nr = game.get_number(position);
+             button(position).Text = nr.ToString();
+                button(position).Visible = (nr <16);
+            }
+               
+        }
+
+      
+
+        
+    }
+}
